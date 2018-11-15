17f2364003e52c9989643d330530bf95881dc7f2
Bryson Nelson <brysonnelson@Brysons-MBP.attlocal.net>
2018-11-14 17:56:16 -0800

    connection db to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ var mysql = require("mysql");
 
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.PORT || 3306,
-  user: "root",
-  password: process.env.MYSQL_PASS,
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: process.env.PORT || 3306,
+        user: "root",
+        password: process.env.MYSQL_PASS,
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
